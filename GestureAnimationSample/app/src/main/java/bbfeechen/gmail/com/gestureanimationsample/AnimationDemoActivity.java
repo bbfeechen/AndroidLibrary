d9f4cf2587b91a9591cb15ba094b803ed37018db
Kailiang Chen <bbfeechen@gmail.com>
2016-02-24 16:34:15 -0800

    add

---
+++ GestureAnimationSample/app/src/main/java/bbfeechen/gmail/com/gestureanimationsample/AnimationDemoActivity.java
@@ -0,0 +1,85 @@
+package bbfeechen.gmail.com.gestureanimationsample;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class AnimationDemoActivity extends AppCompatActivity {
+
+    private Button btnFadeOut;
+    private TextView tvMessage;
+    private Button btnRepeat;
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animation_demo);
+        btnFadeOut = (Button) findViewById(R.id.btnFade);
+        btnRepeat = (Button) findViewById(R.id.btnRepeat);
+        tvMessage = (TextView) findViewById(R.id.tvMessage);
+    }
+
+    public void onFadeOut(View v) {
+        btnFadeOut.animate().alpha(0).setDuration(2000).setListener(new AnimatorListenerAdapter() {
+            @Override public void onAnimationEnd(Animator animation) {
+                btnFadeOut.animate().alpha(1f).setDuration(2000);
+            }
+        });
+    }
+
+    // Slide message from button up to display, then later slide out
+    public void onSlideMessage(View v) {
+        tvMessage.setVisibility(View.VISIBLE);
+        final int yPosInitial = getScreenHeight() + tvMessage.getHeight();
+        int yPosDest = tvMessage.getHeight();
+        tvMessage.setY(yPosInitial);
+        tvMessage.animate()
+            .translationY(yPosDest)
+            .setDuration(2000)
+            .setListener(new AnimatorListenerAdapter() {
+                @Override public void onAnimationEnd(Animator animation) {
+                    tvMessage.animate()
+                        .translationY(yPosInitial)
+                        .setDuration(2000)
+                        .setStartDelay(2000);
+                }
+            });
+    }
+
+    public void onRepeatAnimation(View v) {
+        // Setup scale Y
+        ObjectAnimator scaleAnimY = ObjectAnimator.ofFloat(btnRepeat, "scaleY", 1.0f, 1.5f);
+        scaleAnimY.setDuration(3000);
+        scaleAnimY.setRepeatCount(ValueAnimator.INFINITE);
+        scaleAnimY.setRepeatMode(ValueAnimator.REVERSE);
+        // Setup scale X
+        ObjectAnimator scaleAnimX = ObjectAnimator.ofFloat(btnRepeat, "scaleX", 1.0f, 1.5f);
+        scaleAnimX.setDuration(3000);
+        scaleAnimX.setRepeatCount(ValueAnimator.INFINITE);
+        scaleAnimX.setRepeatMode(ValueAnimator.REVERSE);
+        // Setup animation set
+        AnimatorSet set = new AnimatorSet();
+        set.playTogether(scaleAnimX, scaleAnimY);
+        set.start();
+    }
+
+    public void onActivityTransition(View v) {
+        Intent i = new Intent(AnimationDemoActivity.this, MainActivity.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.slide_right_in, R.anim.slide_left_out);
+    }
+
+    public int getScreenHeight() {
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        return metrics.heightPixels;
+    }
+}
